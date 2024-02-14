d7ac1b7dde88d7a920a1d058451a3924e5fe116a
Devops <Shivalingbiradar9@gmail.com>
2024-02-15 01:41:10 +0530

    This is my first python script

---
+++ QuotesandComments.py
@@ -0,0 +1,23 @@
+# Using single quotes
+single_quoted_string = 'This is a single-quoted string.'
+
+# Using double quotes
+double_quoted_string = "This is a double-quoted string."
+
+multi_line_string = '''
+This is a multi-line string.
+It spans multiple lines.
+It uses triple quotes.
+'''
+
+skill = "DevOps"
+print("skill")
+print('skill')
+print(skill)
+
+print("""
+This is a paragraph string,
+This is the second line of the paragraph,
+third line 
+""")
+
