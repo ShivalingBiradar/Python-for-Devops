89965eac099f3155b37dce0433b9c7f0f297ccb1
Devops <Shivalingbiradar9@gmail.com>
2024-02-15 02:21:13 +0530

    This is my first Datatypes and Variables types

---
+++ variables.py
@@ -0,0 +1,40 @@
+
+# Variable Assignments
+
+var1 = "Python"
+var2 = 75
+var3 = 3.5
+
+# Printing Variables
+print(var1)
+print(var2)
+print(var3)
+
+# Multiple assignments
+
+a = b = c = 65
+
+print(a)
+print(b)
+print(c)
+
+# Multiple Values assigned to Multiple Variables
+
+w, x, y, z = "alpha", "beta", 12, 5.4
+
+# Printing Variables
+
+print("Variable w value is ", w)
+print("Variable x value is ", x)
+print("Variable y value is ", y)
+print("Variable z value is", z)
+
+# print(type(w))
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+print("Variable qw value is ",type(w))
+print("Variable x value is ", type(x))
+print("Variable y value is ",type(y))
+print("Variable z value is", type(z))
