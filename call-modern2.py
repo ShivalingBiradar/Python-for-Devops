2aa23cfff284d79ca4f77b6c78871516d99f5cf9
Devops <Shivalingbiradar9@gmail.com>
2024-02-19 23:10:39 +0530

    This is Modules script.

---
+++ call-modern2.py
@@ -0,0 +1,2 @@
+from modern1 import *
+order_food("Pizza")
---
+++ call-module.py
@@ -0,0 +1,6 @@
+import modern1
+# print(dir(modern1))
+
+modern1.order_food("Salad","Pizza", "Biryani","Soup")
+modern1.vac_feedback("Unknown", 45)
+modern1.time_activity(10, 20, 10, hobby="Dance", sport="Boxing", fun="Driving", work="DevOps")
